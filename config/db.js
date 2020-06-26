23b4ace63b6b2eab1aa3fa3dd075ef4b2a718c79
Mayra Guevara <maystef1@hotmail.com>
2020-06-25 22:00:09 -0600

    Se configuraron los parametros para la base de datos
    Signed-off-by: Mayra Guevara <maystef1@hotmail.com>

---
+++ config/db.js
@@ -0,0 +1,27 @@
+// Importamos el modulo sequelize
+const Sequelize = require("sequelize");
+require("dotenv").config({ path: "variables.env" });
+
+// Parámetros de la conexión a la base de datos
+const db = new Sequelize(
+  "cakeshop",
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASS,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: process.env.MYSQLPORT,
+    operatorAliases: false,
+    define: {
+      timestamps: false,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
+
+module.exports = db;
