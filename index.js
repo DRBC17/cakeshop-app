fb595d761fa3836399f7b52d81311c6f28b2f98f
Diego Buezo <diegobuezo2016@gmail.com>
2020-06-25 21:04:11 -0600

    Se agrego el motor de vista
    Signed-off-by: Diego Buezo <diegobuezo2016@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const express = require("express");
+const path = require('path');
+//handlebars
+const Handlebars = require("handlebars");
+const exphbs = require("express-handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+//End handlebars
+
+
+const app = express();
+
+// >Settings
+
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+  })
+);
+
+app.set("view engine", ".hbs");
+
+// >Middleware
+
+// >Routes
+
+// >Static files
+
+// >Global variable
+
+// Inizialiazar el server
+app.listen(port, () => {
+  console.log(`Server started on port`);
+});
