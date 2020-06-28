908c5303363a9cbed8f4cf87947d1b18b283fce5
Diego Buezo <diegobuezo2016@gmail.com>
2020-06-27 20:15:07 -0600

    Se agrego el CDN para bootstrap modificado y se crearon las carpetas necesarias para hbs
    Signed-off-by: Diego Buezo <diegobuezo2016@gmail.com>

--- routers/index.router.js
+++ routers/index.router.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
-const indexController = require("../controllers/index.controller");
+const indexController = require("../controllers/userscontroller");
 
 module.exports = function () {
   router.get("/", indexController.home);
