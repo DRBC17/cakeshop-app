8ee3dcf4d3bf64ec2a3e8cff5e79bd9f7ab5b869
Diego Buezo <diegobuezo2016@gmail.com>
2020-07-02 16:33:25 -0600

    Se soluciono el problema de de actualizar categorias
    Signed-off-by: Diego Buezo <diegobuezo2016@gmail.com>

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -103,3 +103,84 @@ exports.CrearCategoria = async (req, res, next) => {
     }
   }
 };
+
+// Busca un categoría por su URL
+exports.obtenerCategoriaPorUrl = async (req, res, next) => {
+  try {
+    // Obtener la categoría mediante la URL
+    const categories = await Category.findOne({
+      where: {
+        url: req.params.url,
+      },
+    });
+
+    // Cambiar la visualización de la fecha con Moment.js
+    const created = moment(categories["dataValues"].createdAt).format("LLLL");
+    const updated = moment(categories["dataValues"].updatedAt).fromNow();
+
+    res.render("category/updateCategory", {
+      title: "Categorías | GloboFiestaCake's",
+      authAdmin: "yes",
+      created,
+      updated,
+      categories: categories,
+    });
+  } catch (error) {
+    res.send(error);
+    res.redirect("/categorias");
+  }
+};
+
+// Actualizar los datos de una categoría
+exports.actualizarCategoria = async (req, res, next) => {
+  // Obtener la información enviada
+  const { name, description } = req.body;
+
+  const mensajes = [];
+
+  // Verificar el nombre
+  if (!name) {
+    messages.push({
+      error: "¡Debe ingresar un nombre!",
+      type: "alert-danger",
+    });
+  }
+  // Verificar la descripción
+  if (!description) {
+    messages.push({
+      error: "¡Debe ingresar una descripción!",
+      type: "alert-danger",
+    });
+  }
+
+  // Si hay mensajes
+  if (mensajes.length) {
+    // Enviar valores correctos si la actualización falla
+    categories = await Category.findByPk(req.params.id);
+    // Cambiar la visualización de la fecha con Moment.js
+    const created = moment(categories.createdAt).format("LLLL");
+    const updated = moment(categories.updatedAt).fromNow();
+
+    res.render("updateCategory", {
+      title: "Actualizar categoría | GloboFiestaCake's",
+      authAdmin: "yes",
+      messages,
+      created,
+      updated,
+      categories: categories,
+    });
+  } else {
+    // No existen errores ni mensajes
+    await Category.update(
+      { name, description },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    // Redirigir hacia las categorías
+    res.redirect("/categorias");
+  }
+};
