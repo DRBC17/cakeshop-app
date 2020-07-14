9e73179af7e339cb9d3b3df6df29baa1dcf02d70
Diego Buezo <diegobuezo2016@gmail.com>
2020-07-13 23:19:37 -0600

    Se agrego arg al helper
    Signed-off-by: Diego Buezo <diegobuezo2016@gmail.com>

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,11 +1,12 @@
 module.exports = {
-    //Helper para comparar dos valores
+  //Helper para comparar dos valores
   compare: (value1, value2, options) => {
-    if (value1 != value2) {
-      return options.inverse(this);
-    } else {
-      return options.fn(this);
-    }
+    if (arguments.length < 3)
+      throw new Error("Handlebars Helper compare necesita 2 parámetros.");
+      if (value1 === value2) {
+        return options.fn(this);
+      } else {
+        return options.inverse(this);
+      }
   },
 };
-
